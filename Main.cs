a04e77c52b124f15fbdb208f75e0335081299993
zlpdss <121337669+zlpdss@users.noreply.github.com>
2023-02-01 00:53:07 -0600

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -1,1 +1,147 @@
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using WeAreDevs_API;
 
+namespace indexZ
+{
+    public partial class indexZ : Form
+    {
+        ExploitAPI api = new ExploitAPI();
+        public indexZ()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        Point lastPoint;
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            fastColoredTextBox1.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            api.SendLuaScript(fastColoredTextBox1.Text);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            api.LaunchExploit();
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                openFileDialog1.Title = "Open";
+                fastColoredTextBox1.Text = File.ReadAllText(openFileDialog1.FileName);
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                using (Stream s = File.Open(saveFileDialog1.FileName, FileMode.CreateNew))
+                using (StreamWriter sw = new StreamWriter(s))
+                {
+                    sw.Write(fastColoredTextBox1.Text);
+                }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            fastColoredTextBox1.Text = File.ReadAllText($"./scripts/{listBox1.SelectedItem}");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();//Clear Items in the LuaScriptList
+            functions.PopulateListBox(listBox1, "./Scripts", "*.txt");
+            functions.PopulateListBox(listBox1, "./Scripts", "*.lua");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click_1(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Maximized;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var versioncheck = new System.Net.WebClient();
+            if (!versioncheck.DownloadString("https://github.com/zlpdss/indexZ/blob/main/updateVersion").Contains("1.2"))
+            {
+                label2.Text = "Out-Dated";
+                label2.ForeColor = Color.FromArgb(192, 0, 0);
+            }
+            else
+            {
+                label2.Text = "Up-To-Date";
+                label2.ForeColor = Color.FromArgb(255, 255, 192);
+            }
+        }
+
+        private void fastColoredTextBox1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
